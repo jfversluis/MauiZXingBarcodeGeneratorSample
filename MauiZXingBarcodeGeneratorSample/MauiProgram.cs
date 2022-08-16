e73436ae3f9a5bb0887c05c13c2331ae9eab7641
Gerald Versluis <gerald@verslu.is>
2022-08-16 15:53:31 +0200

    Implement barcode generation

--- MauiZXingBarcodeGeneratorSample/MauiProgram.cs
+++ MauiZXingBarcodeGeneratorSample/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiZXingBarcodeGeneratorSample;
+﻿using ZXing.Net.Maui;
+
+namespace MauiZXingBarcodeGeneratorSample;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseBarcodeReader()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
